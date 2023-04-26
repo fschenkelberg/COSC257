2b6858b2ecbb949706e96431f42a9dc9da2e322e
fschenkelberg <felicia.schenkelberg.th@dartmouth.edu>
2023-04-25 23:16:19 -0400

    week2_p1

---
+++ week2_p1 /miniC/main.c
@@ -0,0 +1,17 @@
+extern int func(int);
+
+void print(int n){
+	printf("%d\n", n);
+}
+
+int read(){
+	int n;
+	scanf("%d",&n);
+	return(n); 
+}
+
+int main(){
+ int a = func(20);
+ printf("%d", a);
+}
+
